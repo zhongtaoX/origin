1aae76f5c803692c2444dd32b171f87a41ec542e
zhongtao <18269776130@163.com>
2023-02-22 23:08:20 +0800

    feat(项目搭建): 项目搭建追加
    项目搭建与第二次提交到本地库

--- src/main/java/com/example/GitTest.java
+++ src/main/java/com/example/GitTest.java
@@ -3,5 +3,6 @@ package com.example;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello Git!");
     }
 }
