86766e0f191eec417c435273a3ca4c8bf2674485
zhongtao <18269776130@163.com>
2023-02-28 23:43:02 +0800

    fix(GitTest): 添加语句
    System.out.println("Hello Git3!");
    
    Signed-off-by: zhongtao <18269776130@163.com>

--- src/main/java/com/example/GitTest.java
+++ src/main/java/com/example/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("Hello Git!");
         System.out.println("Hello Git1!");
         System.out.println("Hello Git2!");
+        System.out.println("Hello Git3!");
     }
 }
